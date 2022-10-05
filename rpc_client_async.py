3d880b9e49dd723fc8a579616810266c696ea0e7
faceless_void <sauravsust71@gmail.com>
2022-10-05 02:01:58 -0500

    rpc deferred async done

---
+++ rpc_client_async.py
@@ -0,0 +1,121 @@
+from copyreg import pickle
+from secrets import choice
+from urllib import response
+from config import config_dict
+import socket 
+import json 
+import os 
+from base64 import b64encode
+import random 
+import numpy as np 
+from ast import literal_eval
+import threading
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+CLIENT_FILE_DIR = 'client_file_dir'
+
+def get_queued_results(s):
+    # while True:
+    message = json.dumps({"func_name": 'queued_result'}).encode()
+    s.sendall(message) 
+    push_recieve = s.recv(1048576)
+    
+    if push_recieve:
+        message = json.loads(push_recieve)
+        if message.get('message', None):
+            print(message['message'])
+            return 
+        print('\n')
+        print(f"{len(message.keys())} Push notification recieved ")
+        print('-' * 20)
+        print(json.loads(push_recieve))
+        return
+    return 
+
+def calculate_pi(s):
+    message = json.dumps({"func_name": 'calculate_pi'}).encode()
+    s.sendall(message)
+    response = s.recv(1024)
+    print(f'ACK recieved: {response.decode()}')
+    if len(response) < 2:
+        raise Exception('Lost connection')
+    return 
+
+def add(s):
+    a,b = input('Please enter the two numbers seperated by space\n').split(' ')
+    message = json.dumps({"func_name": 'add', 'a': a, 'b': b}).encode()
+    s.sendall(message)
+    response = s.recv(1024)
+    print(f'ACK recieved: {response.decode()}')
+    print('\n')
+    if len(response) < 2:
+        raise Exception('Lost connection')
+    
+
+def sort(s):
+    array_size = input('Please enter size of the array you want to sort')
+    randomlist = random.sample(range(0, 500), int(array_size))
+    message = json.dumps({"func_name": 'sort', 'array': randomlist}).encode()
+    s.sendall(message)
+    response = s.recv(1048576)
+    print(f'ACK recieved: {response.decode()}')
+    if len(response) < 2:
+        raise Exception('Lost connection')
+    
+
+def matrix_multiply(s):
+    matrices = input('Please enter the two tuples seperated by space indicating matrix size (r1,c1) (r2,c2) ').split(' ')
+    print(matrices)
+    mat_a_shape, mat_b_shape = matrices
+    r1, c1 = literal_eval(mat_a_shape)
+    r2, c2 = literal_eval(mat_b_shape)
+    
+    r1 = int(r1)
+    c1 = int(c1)
+    r2 = int(r2)
+    c2 = int(c2)
+    
+    print(r1, c1, r2, c2)
+    mat_a = np.random.rand(r1, c1)
+    mat_b = np.random.rand(r2, c2)
+    if c1 != r2:
+        print('Invalid shape for matrix multiplication')
+    
+    message = json.dumps({"func_name": 'matrix_multiply', 'mat_a': mat_a, 'mat_b': mat_b}, cls=NumpyEncoder).encode()
+    s.sendall(message)
+    response = s.recv(1048576)
+    if len(response) < 2:
+        raise Exception('Lost connection')
+    print(f'ACK recieved: {response.decode()}')
+    
+
+if __name__ == '__main__':
+    function_dict = {1: calculate_pi, 2: add, 3: sort, 4: matrix_multiply, 5: get_queued_results}
+    
+    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s =socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    s.connect((config_dict['host'], config_dict['port']))
+    print('Connected to server \n\n')
+    while True:
+        print('\n')
+        print('------------------Starts again------------')
+        print('Please select one from the below operations')
+        options = {
+            1: 'calculate_pi', 2: 'add', 3: 'sort', 4: 'matrix_multiply', 5: 'Get Queued Results from server'
+        }
+        print(f'{options}')
+        print('\n')
+        selection = int(input('').strip())
+        function_dict[selection](s)
+        # print('Operation Successful')
+        # break
--- rpc_server_async_deferred.py
+++ rpc_server_async_deferred.py
@@ -8,10 +8,19 @@ from base64 import b64decode
 import os 
 import traceback
 import threading
-import pickle 
+import pickle
+from unittest import result 
+import time 
 
 from config import config_dict
 import numpy as np
+from datetime import datetime
+
+def get_current_time_now():
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
 
 class NumpyEncoder(json.JSONEncoder):
     """ Special json encoder for numpy types """
@@ -33,6 +42,7 @@ class Server(object):
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
         self.function_dict = {'calculate_pi': self.calculate_pi, 'add': self.add, 'sort': self.sort, 'matrix_multiply': self.matrix_multiply}
+        self.client_async_results = []
     
     def listen(self):
         self.sock.listen(10)
@@ -40,6 +50,7 @@ class Server(object):
             try:
                 client, address = self.sock.accept()
                 client.settimeout(60)  #Idle timeout
+                
                 threading.Thread(target = self.talk_to_client,args = (client,address)).start()
                 
             except:
@@ -72,29 +83,60 @@ class Server(object):
             k+=2
         
         sum_string = str(s)
-        client.send(sum_string.encode())
+        client.send(b'ack')
+        
+        self.client_async_results.append({
+            'client': client, 'time': get_current_time_now(), 'func': 'cl_pi', 'res' : sum_string
+        })
+        # client.send(sum_string.encode())
         
     def add(self, client, data):
         a = data['a']
         b = data['b']
-        
-        client.send(str(int(a) + int(b)).encode())
+        client.send(b'ack')
+        result = str(int(a) + int(b))
+        self.client_async_results.append({
+            'client': client, 'time': get_current_time_now(), 'func': 'add', 'res' : result
+        })
+        # client.send(str(int(a) + int(b)).encode())
             
     def sort(self, client, data):
         array = sorted(data['array'])
         result = json.dumps(array)
-        client.send(result.encode())
+        # client.send(result.encode())
+        client.send(b'ack')
+        self.client_async_results.append({
+            'client': client, 'time': get_current_time_now(), 'func': 'sort', 'res' : result
+        })
     
     def matrix_multiply(self, client, data):
         mat_a = data['mat_a']
         mat_b = data['mat_b']
         print(type(mat_a), type(mat_b))
         result = np.dot(mat_a, mat_b) 
-        client.send(json.dumps(result, cls=NumpyEncoder).encode())
+        client.send(b'ack')
+        result = json.dumps(result, cls=NumpyEncoder)
+        self.client_async_results.append({
+            'client': client, 'time': get_current_time_now(), 'func': 'matrix_multiply', 'res' : result
+        })
+        
+        
+    def push_send(self, server, id):
+        while True:
+            if len(server.client_async_results) > 0:
+                server_result = server.client_async_results.pop()
+                client = server_result['client']
+                del server_result['client']
+                client.send(json.dumps(server_result).encode())
+            time.sleep(10)
+            
         
 
 if __name__ == '__main__':
     server = Server(config_dict=config_dict)
+    threading.Thread(target = server.push_send,args = (server, 0)).start()
     server.listen()
+    
+        
 
 #sudo lsof -t -i tcp:8081 | sudo xargs kill
