641e3fd6a3ad648baed83bb7a2a12a4ed4f25d0a
faceless_void <sauravsust71@gmail.com>
2022-10-04 22:06:12 -0500

    rpc server done

---
+++ rpc_client.py
@@ -0,0 +1,92 @@
+from copyreg import pickle
+from secrets import choice
+from urllib import response
+from config import config_dict
+import socket 
+import json 
+import os 
+from base64 import b64encode
+import random 
+import numpy as np 
+from ast import literal_eval
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+CLIENT_FILE_DIR = 'client_file_dir'
+
+def calculate_pi(s):
+    message = json.dumps({"func_name": 'calculate_pi'}).encode()
+    s.sendall(message)
+    response = s.recv(1024)
+    print(f'PI result {response.decode()}')
+    if len(response) < 4:
+        raise Exception('Lost connection')
+
+def add(s):
+    a,b = input('Please enter the two numbers seperated by space\n').split(' ')
+    message = json.dumps({"func_name": 'add', 'a': a, 'b': b}).encode()
+    s.sendall(message)
+    response = s.recv(1048576)
+    if len(response) < 2:
+        raise Exception('Lost connection')
+    
+
+def sort(s):
+    array_size = input('Please enter size of the array you want to sort')
+    randomlist = random.sample(range(0, 500), int(array_size))
+    message = json.dumps({"func_name": 'sort', 'array': randomlist}).encode()
+    s.sendall(message)
+    response = s.recv(1024)
+    if len(response) < 4:
+        raise Exception('Lost connection')
+    print(f'Sorted Array {response.decode()}')
+
+def matrix_multiply(s):
+    matrices = input('Please enter the two tuples seperated by space indicating matrix size (r1,c1) (r2,c2) ').split(' ')
+    print(matrices)
+    mat_a_shape, mat_b_shape = matrices
+    r1, c1 = literal_eval(mat_a_shape)
+    r2, c2 = literal_eval(mat_b_shape)
+    
+    r1 = int(r1)
+    c1 = int(c1)
+    r2 = int(r2)
+    c2 = int(c2)
+    
+    print(r1, c1, r2, c2)
+    mat_a = np.random.rand(r1, c1)
+    mat_b = np.random.rand(r2, c2)
+    if c1 != r2:
+        print('Invalid shape for matrix multiplication')
+    
+    message = json.dumps({"func_name": 'matrix_multiply', 'mat_a': mat_a, 'mat_b': mat_b}, cls=NumpyEncoder).encode()
+    s.sendall(message)
+    response = s.recv(1024)
+    if len(response) < 4:
+        raise Exception('Lost connection')
+    print(f'matrix multiplication result {response.decode()}')
+    
+
+if __name__ == '__main__':
+    function_dict = {1: calculate_pi, 2: add, 3: sort, 4: matrix_multiply}
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((config_dict['host'], config_dict['port']))
+        print('Connected to server \n\n')
+        while True:
+            print('Please select one from the below operations')
+            options = {
+                1: 'calculate_pi', 2: 'add', 3: 'sort', 4: 'matrix_multiply'
+            }
+            print(f'{options}')
+            selection = int(input('').strip())
+            function_dict[selection](s)
+            print('Operation Successful')
---
+++ rpc_server.py
@@ -0,0 +1,100 @@
+
+from genericpath import isfile
+from http import client
+import socket
+import json 
+from ast import literal_eval
+from base64 import b64decode
+import os 
+import traceback
+import threading
+import pickle 
+
+from config import config_dict
+import numpy as np
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+class Server(object):
+    def __init__(self, config_dict) -> None:
+        self.host = config_dict['host']
+        self.port = config_dict['port']
+        self.server_dir = config_dict['server_dir']
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+        self.function_dict = {'calculate_pi': self.calculate_pi, 'add': self.add, 'sort': self.sort, 'matrix_multiply': self.matrix_multiply}
+    
+    def listen(self):
+        self.sock.listen(10)
+        while True:
+            try:
+                client, address = self.sock.accept()
+                client.settimeout(60)  #Idle timeout
+                threading.Thread(target = self.talk_to_client,args = (client,address)).start()
+                
+            except:
+                raise Exception('Server stopped')
+    
+    def talk_to_client(self, client, address):
+        size = 1048576
+        while True:
+            try:
+                data = client.recv(size).decode('utf-8')
+                if len(data) < 10:
+                    continue
+                data = literal_eval(data)
+                func_name = data['func_name']
+                self.function_dict[func_name](client, data)
+
+            except:
+                traceback.print_exc()
+                print('Client lost the connection ')
+                break
+        
+    def calculate_pi(self, client, data):
+        k = 1
+        s = 0
+        for i in range(1000000):
+            if i%2 == 0:
+                s += 4/k
+            else:
+                s -= 4/k
+            k+=2
+        
+        sum_string = str(s)
+        client.send(sum_string.encode())
+        
+    def add(self, client, data):
+        a = data['a']
+        b = data['b']
+        
+        client.send(str(int(a) + int(b)).encode())
+            
+    def sort(self, client, data):
+        array = data['array']
+        result = json.dumps(array)
+        client.send(result.encode())
+    
+    def matrix_multiply(self, client, data):
+        mat_a = data['mat_a']
+        mat_b = data['mat_b']
+        print(type(mat_a), type(mat_b))
+        result = np.dot(mat_a, mat_b) 
+        client.send(json.dumps(result, cls=NumpyEncoder).encode())
+        
+
+if __name__ == '__main__':
+    server = Server(config_dict=config_dict)
+    server.listen()
+
+#sudo lsof -t -i tcp:8081 | sudo xargs kill
